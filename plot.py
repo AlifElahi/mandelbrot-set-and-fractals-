c0eccffce3a080e8f98902389f05bf104e7c0433
alifelahikhan <alifelahikhan@gmail.com>
2021-04-25 16:55:09 +0600

    first commit

---
+++ plot.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageDraw
+from mandelbrot import mandelbrot, MAX_ITER
+
+# Image size (pixels)
+WIDTH = 600
+HEIGHT = 400
+
+# Plot window
+RE_START = -2
+RE_END = 1
+IM_START = -1
+IM_END = 1
+palette = []
+
+c=complex(0.285,0.01)
+
+im = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        # Convert pixel coordinate to complex number
+        z1 = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
+        # Compute the number of iterations
+        m = mandelbrot(c,z1)
+        # The color depends on the number of iterations
+        color = 255 - int(m * 255 / MAX_ITER)
+        # Plot the point
+        draw.point([x, y], (color, color, color))
+
+im.save('output.png', 'PNG')
+im.show()
+print('hello')
